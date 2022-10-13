fea7fdd01bb5033437d99b93e23ce62f9095a1ac
sushobhit-lt <sushobhitd@lambdatest.com>
2022-10-13 18:15:24 +0000

    add cases to fetch browser logs and take screenshot

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -12,6 +12,18 @@ import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import java.util.logging.Level;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import java.io.File;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class TestNGTodo2 {
 
@@ -32,7 +44,7 @@ public class TestNGTodo2 {
         Note: for additional capabilities navigate to https://www.lambdatest.com/support/docs/test-settings-options/
         */
 
-        String hub = "@hub.lambdatest.com/wd/hub";
+        String hub = "@stage-hub.lambdatest.com/wd/hub";
 
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platform", "Windows 10");
@@ -41,7 +53,11 @@ public class TestNGTodo2 {
         caps.setCapability("build", "TestNG With Java");
         caps.setCapability("name", m.getName() + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
+        LoggingPreferences logPrefs = new LoggingPreferences();
+        logPrefs.enable(LogType.BROWSER, Level.ALL);
 
+        caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+       
         /*
         Enable Smart UI Project
         caps.setCapability("smartUI.project", "<Project Name>");
@@ -71,42 +87,20 @@ public class TestNGTodo2 {
 
         System.out.println("Checking Another Box");
         driver.findElement(By.name("li4")).click();
+        
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        for (LogEntry entry : logEntries) {
+            System.out.println(entry.getTimestamp() + " " + entry.getLevel() + " " + entry.getMessage());
+            //do something useful with the data
+        }
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
-        driver.findElement(By.id("addbutton")).click();
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot =((TakesScreenshot)driver);
+        //Call getScreenshotAs method to create image file
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
-        driver.findElement(By.id("addbutton")).click();
-
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
-        driver.findElement(By.id("addbutton")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li1")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li3")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li7")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li8")).click();
-
-        System.out.println("Entering Text");
-        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
-
-        driver.findElement(By.id("addbutton")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li9")).click();
-
-        // Let's also assert that the todo we added is present in the list.
-
-        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
-        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
         Status = "passed";
-        Thread.sleep(150);
+        Thread.sleep(1500);
 
         System.out.println("TestFinished");
 
