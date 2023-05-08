6f8d21268d33844ce790547d12a98da9c3be670d
sushobhit-lt <sushobhitd@lambdatest.com>
2023-05-08 11:32:01 +0000

    update sample code

--- src/test/java/com/lambdatest/GuideslySample.java
+++ src/test/java/com/lambdatest/GuideslySample.java
@@ -37,8 +37,9 @@ public class GuideslySample {
         caps.setCapability("build", "TestNG With Java");
         caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
-        caps.setCapability("smartUI.project", "testng-smartui-project");
-        caps.setCapability("selenium_version", "4.8.0");
+        caps.setCapability("smartUI.project", "guidesly-project");
+        caps.setCapability("selenium_version", "4.0.0");
+        // caps.setCapability("smartUI.baseline", true);
 
         if (githubURL != null) {
             Map < String, String > github = new HashMap < String, String > ();
@@ -53,33 +54,93 @@ public class GuideslySample {
     @Test
     public void basicTest() throws InterruptedException {
         String spanText;
-        System.out.println("Loading Url");
 
-        driver.get("https://guidesly.com/book-a-guide/guide-details/Tuna-Wahoo-Charters?tripDate=2023-04-21&guests=1");
+        //Step 1
+        System.out.println("Loading Home  Page");
+
+        driver.get("https://guidesly.com");
         Thread.sleep(2000);
-        scrollToBottom();
-
-        driver.executeScript("smartui.takeFullPageScreenshot=fullpage");
+        scrollToBottom(2000);
+        driver.executeScript("smartui.takeFullPageScreenshot=home-page");
+        System.out.println("home-page Saved");
         Thread.sleep(1000);
 
-        System.out.println("TestFinished");
+        //Step 2
+        System.out.println("Loading Bio Page");
+
+        driver.get("https://guidesly.com/book-a-guide/guide-details/Tuna-Wahoo-Charters?tripDate=2023-04-21&guests=1");
+        scrollToBottom(2000);
+
+        driver.executeScript("smartui.takeFullPageScreenshot=guide-bio-page");
+        Thread.sleep(1000);
+
+        System.out.println("guide-bio-page Saved");
+        //Step 3
+        System.out.println("Loading Search  Page");
+
+        driver.get("https://guidesly.com/search?address=Tambaram%2C+Tamil+Nadu&latitude=12.94336&longitude=80.1570816&guest=1&category=alltrips&page=1");
+        Thread.sleep(2000);
+        quickScrollToBottom(5000);
+        driver.executeScript("smartui.takeFullPageScreenshot=search-page");
+        Thread.sleep(1000);
+
+        System.out.println("search-page Saved");
+
 
     }
 
-    public void scrollToBottom() {
+    public void scrollToBottom(int lastPageWait) {
         try {
             Long height = getJavaScriptReturnValue("document.body.scrollHeight");
             System.out.println("height: " + height);
             int heightOfPage = height.intValue();
             System.out.println("heightOfPage: " + heightOfPage);
 
-            int noOfLoop = heightOfPage / 400;
-            for (int i = 0; i < noOfLoop; i++) {
-                ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, " + (i * 400) + ")");
+            int size = 500;
+            int noOfLoop = heightOfPage / size;
+            System.out.println("noOfLoop: " + noOfLoop);
+            for (int i = 1; i <= noOfLoop; i++) {
+                ((JavascriptExecutor) driver).executeScript("window.scrollTo(" + (i - 1 * size) + "," + (i * size) + ")");
                 Thread.sleep(1000);
+                if (i == noOfLoop) {
+                    System.out.println("End of loop : " + i * size + "to " + heightOfPage);
+                    ((JavascriptExecutor) driver).executeScript("window.scrollTo(" + (i * size) + "," + heightOfPage + ")");
+                    Thread.sleep(lastPageWait);
+                }
             }
-            Thread.sleep(1000);
-            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, " + heightOfPage + ")");
+            // Now scroll at the top
+            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,0)");
+            Thread.sleep(5000);
+            System.out.println("Scroll Completed");
+        } catch (Exception e) {
+            System.out.println("Got some errors" + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+
+    public void quickScrollToBottom(int lastPageWait) {
+        try {
+            Long height = getJavaScriptReturnValue("document.body.scrollHeight");
+            System.out.println("height: " + height);
+            int heightOfPage = height.intValue();
+            System.out.println("heightOfPage: " + heightOfPage);
+
+            int size = 200;
+            int noOfLoop = heightOfPage / size;
+            System.out.println("noOfLoop: " + noOfLoop);
+            for (int i = 1; i <= noOfLoop; i++) {
+                ((JavascriptExecutor) driver).executeScript("window.scrollTo(" + (i - 1 * size) + "," + (i * size) + ")");
+                Thread.sleep(1000);
+                if (i == noOfLoop) {
+                    System.out.println("End of loop : " + i * size + "to " + heightOfPage);
+                    ((JavascriptExecutor) driver).executeScript("window.scrollTo(" + (i * size) + "," + heightOfPage + ")");
+                    Thread.sleep(lastPageWait);
+                }
+            }
+            // Now scroll at the top
+            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,0)");
+            Thread.sleep(10000);
             System.out.println("Scroll Completed");
         } catch (Exception e) {
             System.out.println("Got some errors" + e.toString());
